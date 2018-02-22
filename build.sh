df7412cf951e916e3a5042884bd3ce7a891e700a
Ralf Coenen <ralf@debian.localhost>
2018-02-22 07:22:28 +0100

    Mit eigenem Git-Repo

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cwd="$(pwd)"
+buildpath="$(lektor project-info --output-path)"
+
+lektor build
+
+cd $buildpath
+git add .
+echo "Bitte geben Sie die Commit-Message ein:"
+read msg
+git commit -am "$msg"
+git push origin master
+read -n 1 -s
+cd $cwd
